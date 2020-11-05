70c7384765a7917bae3b9fc9eea5109a5ab35331
Tony Wang <guanwan@microsoft.com>
2020-11-05 17:42:37 +0800

    add demo

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,13 +9,14 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <p>Welcome to our FHL project.     <Link to="/vogd/">Go to See demo</Link> <br /></p>
+    <div style={{ maxWidth: `900px`, marginBottom: `5.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    {/* <Link to="/page-2/">Go to See demo</Link> <br />
+    <Link to="/page-2/">Go to See demo</Link> <br /> */}
+
+    {/* <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
   </Layout>
 )
 
